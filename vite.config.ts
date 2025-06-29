db0b6e84328d5deaa1c097d9d7bcd129c8ad61c5
sashauly <77851963+sashauly@users.noreply.github.com>
2025-06-29 16:58:17 +0300

    Update project name and README for Web Speech API demo
    - Changed project name in package.json from "web-speech" to "react-web-speech".
    - Added homepage URL to package.json.
    - Revised README.md to reflect the new focus on the Web Speech API, providing a brief description of the demo and its features.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (e.g., 'development', 'production')
+  // By default, `loadEnv` will load .env files
+  const env = loadEnv(mode, process.cwd(), "");
+
+  // Get the base path from an environment variable, defaulting to '/'
+  // Ensure your CI/CD sets VITE_BASE_URL to '/<REPO>/'
+  const BASE_URL = env.VITE_BASE_URL || "/";
+  return {
+    base: BASE_URL,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
